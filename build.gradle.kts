20ed089da3b03080c60bb109145580875aaa3a37
Raphael Rios <rios.raph@gmail.com>
2025-10-06 22:36:28 -0300

    feat: modify build.gradle.kts for mysql implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java")
 }
 
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
 group = "com.raphael"
 version = "1.0-SNAPSHOT"
 
@@ -10,9 +16,16 @@ repositories {
 }
 
 dependencies {
+    implementation("com.mysql:mysql-connector-j:9.4.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.36")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
 }
 
 tasks.test {
